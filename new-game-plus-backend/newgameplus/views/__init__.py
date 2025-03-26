99905109b79cb7b471bb254497b98d9bad9c5ae1
MatthewHilliard <matthewhilliard2003@gmail.com>
2025-03-26 14:59:53 -0400

    Finished working API flow

---
+++ new-game-plus-backend/newgameplus/views/__init__.py
@@ -0,0 +1,3 @@
+# views/__init__.py
+
+# In order to have seperate views files, django requires this file
--- new-game-plus-frontend/app/page.tsx
+++ new-game-plus-frontend/app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+'use client'
+
+import DemoAPI from "@/api/DemoAPI";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [demoText, setDemoText] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getDemoText = async () => {
+      try {
+        const fetchedDemoText = await DemoAPI.demoGetRequest();
+        setDemoText(fetchedDemoText);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    void getDemoText();
+  }, []);
+
   return (
     <div>
-      New Game Plus
+      <h1>New Game Plus</h1>
+      <h2>{demoText ? demoText : "Loading demo text"}</h2>
     </div>
   );
 }
